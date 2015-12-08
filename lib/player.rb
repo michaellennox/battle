68d99f5be76797bdae1417c8f6523900cbdaff01
michaellennox <michaelctlennox@gmail.com>
2015-12-08 13:14:43 +0000

    Passes previous 2 unit tests for change of HP

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hp
 
   def initialize(name)
     @name = name
+    @hp = 100
+  end
+
+  def attacked
+    @hp -= 20
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,10 +7,16 @@ describe Player do
   it 'should initialize with HP equal to 100' do
     expect(player.hp).to eq 100
   end
-  
+
   describe '#name' do
     it 'should return the players name' do
       expect(player.name).to eq 'Ed'
+    end
+  end
+
+  describe '#attacked' do
+    it 'should reduce HP by 20' do
+      expect{player.attacked}.to change{player.hp}.by (-20)
     end
   end
 
