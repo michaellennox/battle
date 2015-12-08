7d3781b1a4fbf3104ca02e2006645a15184d7cb4
michaellennox <michaelctlennox@gmail.com>
2015-12-08 13:23:00 +0000

    Moved attack_reduce_hp_spec into attack_spec.rb

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,10 +1,20 @@
+feature 'Attack' do
   # As Player 1,
   # So I can win a game of Battle,
   # I want to attack Player 2, and I want to get a confirmation
-feature 'Attack' do
   scenario 'attacking player 2' do
     sign_in_and_play
     click_button('Attack')
     expect(page).to have_content "Ed attacks Michael"
+  end
+
+
+  # As Player 1,
+  # So I can start to win a game of Battle,
+  # I want my attack to reduce Player 2's HP
+  scenario 'attacking reduces HP' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content('Michael HP: 80')
   end
 end
