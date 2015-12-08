c74b6ed7c7a78145cfd35e158545e1b7ae61233a
michaellennox <michaelctlennox@gmail.com>
2015-12-08 13:21:09 +0000

    Passed feature test

--- app.rb
+++ app.rb
@@ -18,15 +18,17 @@ class Battle < Sinatra::Base
   get '/play' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
+    @player_2_hp = $player_2.hp
     @last_action = session[:last_action]
     erb :play
   end
 
   post '/attack' do
     session[:last_action] = :attack
+    $player_2.attacked
     redirect '/play'
   end
-  
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
