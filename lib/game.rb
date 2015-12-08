317b26b27fff811d63cb457a8961ffd49452cd14
michaellennox <michaelctlennox@gmail.com>
2015-12-08 15:34:29 +0000

    Passes all tests for new game class functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,22 @@
 class Game
 
+  attr_reader :player1, :player2
+
   def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
   end
 
   def attack(player)
     player.attacked
+  end
+
+  def player_name(player)
+    player.name
+  end
+
+  def player_hp(player)
+    player.hp
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,8 @@ require 'game'
 describe Game do
 
   subject(:game) { described_class.new(player1, player2) }
-  let(:player1) { double :player }
-  let(:player2) { double :player }
+  let(:player1) { double(:player, name: "Michael", hp: 100) }
+  let(:player2) { double(:player) }
 
   describe '#attack' do
     it 'should call attacked on a player' do
@@ -22,6 +22,18 @@ describe Game do
   describe '#player2' do
     it 'should return player2' do
       expect(game.player2).to eq(player2)
+    end
+  end
+
+  describe '#player_name' do
+    it 'should return the name of player' do
+      expect(game.player_name(player1)).to eq("Michael")
+    end
+  end
+
+  describe '#player_hp' do
+    it 'should return the player\'s current HP' do
+      expect(game.player_hp(player1)).to eq 100
     end
   end
 
