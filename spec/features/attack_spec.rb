488540f03dbc4813ccb0f976ad8c688bd19b3c98
edwardkerry <edwardkerry@gmail.com>
2015-12-08 17:04:13 +0000

    Stage 28 User Stories tested and implemented

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -17,4 +17,23 @@ feature 'Attack' do
     2.times {click_button('Attack')}
     expect(page).to have_content('Michael HP: 80')
   end
+
+  # As Player 1,
+  # So I can lose a game of Battle,
+  # I want Player 2 to attack me, and I want to get a confirmation
+  scenario 'player 2 attacking' do
+    sign_in_and_play
+    2.times {click_button('Attack')}
+    expect(page).to have_content('Michael attacks Ed')
+  end
+
+  # As Player 1,
+  # So I can start to lose a game of Battle,
+  # I want Player 2's attack to reduce my HP
+  scenario 'player 2 attacking reduces player 1 hp' do
+    sign_in_and_play
+    3.times {click_button('Attack')}
+    expect(page).to have_content('Ed HP: 80')
+  end
+
 end
--- spec/features/user_hit_points_spec.rb
+++ spec/features/user_hit_points_spec.rb
@@ -4,9 +4,18 @@
   # So I can see how close I am to winning
   # I want to see Player 2's Hit Points
 
+  # As Player 1,
+  # So I can see how close I am to losing,
+  # I want to see my own hit points
+
 feature 'Hit points' do
-  scenario 'viewing hit points' do
+  scenario 'viewing player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Michael HP: 100'
   end
+  scenario 'viewing player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Ed HP: 100'
+  end
+
 end
