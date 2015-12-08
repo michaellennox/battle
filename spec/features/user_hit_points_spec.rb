7ef925c4c21d121995d01dab754509e67ee1573c
michaellennox <michaelctlennox@gmail.com>
2015-12-08 12:31:44 +0000

    Extracts sign in and play to helpers

--- spec/features/user_hit_points_spec.rb
+++ spec/features/user_hit_points_spec.rb
@@ -6,10 +6,7 @@
 
 feature 'Hit points' do
   scenario 'viewing hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ed'
-    fill_in :player_2_name, with: 'Michael'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Michael HP: 100'
   end
 end
--- spec/features/user_name_spec.rb
+++ spec/features/user_name_spec.rb
@@ -4,10 +4,7 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ed'
-    fill_in :player_2_name, with: 'Michael'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ed vs. Michael'
   end
 end
