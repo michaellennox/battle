e1b31171255a0208e8d3aff6bb48d74833dc678b
edwardkerry <edwardkerry@gmail.com>
2015-12-08 12:53:40 +0000

    Basic attack player 2 button implemented

--- app.rb
+++ app.rb
@@ -17,9 +17,14 @@ class Battle < Sinatra::Base
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    @last_action = session[:last_action]
     erb :play
   end
 
+  post '/attack' do
+    session[:last_action] = :attack
+    redirect '/play'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,10 @@
+  # As Player 1,
+  # So I can win a game of Battle,
+  # I want to attack Player 2, and I want to get a confirmation
+feature 'Attack' do
+  scenario 'attacking player 2' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content "Ed attacks Michael"
+  end
+end
